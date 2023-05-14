a1147748871e5f901cab7277a16cd715a67fc623
SempaiDarcy <mr_darcy_1@mail.ru>
2023-05-15 01:10:05 +0500

    Installing additional libraries. Connecting Less and Craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+import type { FC } from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
+import './App.less';
+import styled from "styled-components";
 
-function App() {
-  return (
+const StyledButton = styled(Button)`
+  border: 2px solid red;
+`
+const App: FC = () => (
     <div className="App">
-
+        <StyledButton type="primary">Test</StyledButton>
     </div>
-  );
-}
+);
 
 export default App;
